463f95b0cf3228ca4db20001f883eb71a1659569
Gaurav Prakash Pareek <gppareek22@gmail.com>
2022-01-04 18:25:02 +0530

    Files Updated

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import streamlit as st
 import requests
 import json
+# from win32com.client import Dispatch
+import pyttsx3
 def speak(str1):
-    from win32com.client import Dispatch
-    speak=Dispatch("SAPI.spvoice")
-    speak.speak(str1)
+    # speak=Dispatch("SAPI.spvoice")
+    # speak.speak(str1)
+    engine = pyttsx3.init()
+    engine.say(str1)
+    engine.runAndWait()
 
 st.title('NEWS For The Day')
+# speak=pyttsx3.init()
 speak("NEWS for today.......")
 url="http://newsapi.org/v2/top-headlines?country=in&apiKey=bf905beed1f440b780221fa6085b42f6"
 t=requests.get(url=url).text
